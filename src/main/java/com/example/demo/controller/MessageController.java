3fd61543de5bae10d5a8b3ab32dd45e0ffa214c7
seungjin <xeungjin@nate.com>
2026-07-07 15:26:39 +0900

    338 page

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,12 @@ public class MessageController {
     @GetMapping("/message/basic")
     public String getMessageBasic() {
         return "message/message-basic";
+    }
+
+    @GetMapping("/message/customer")
+    public String getMessageCustomer(Model model) {
+        model.addAttribute("name", "한빛미디어");
+        model.addAttribute("phone", "02-325-0384");
+        return "message/message-customer";
     }
 }
